d9889a0d624055af4f968a4f8d71eeae13f2e4e7
Bryce Hull <bahull@live.com>
2019-02-16 12:23:45 -0600

    Initial card drag implemented

--- src/Deck.js
+++ src/Deck.js
@@ -4,14 +4,36 @@ import { View, Animated, PanResponder } from "react-native";
 class Deck extends Component {
   constructor(props) {
     super(props);
+    let position = new Animated.ValueXY();
 
-    const panResponder = PanResponder.create({});
+    const panResponder = PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onPanResponderMove: (event, gesture) => {
+        // debugger;
+        position.setValue({ x: gesture.dx, y: gesture.dy });
+      },
+      onPanResponderRelease: () => {}
+    });
 
     this.panResponder = panResponder;
+    this.position = position;
   }
+
   renderCards() {
-    return this.props.data.map(item => {
-      return this.props.renderCard(item);
+    return this.props.data.map((item, index) => {
+      if (index === 0) {
+        return (
+          <Animated.View
+            key={index}
+            style={this.position.getLayout()}
+            {...this.panResponder.panHandlers}
+          >
+            {this.props.renderCard(item)}
+          </Animated.View>
+        );
+      } else {
+        return this.props.renderCard(item);
+      }
     });
   }
   render() {
